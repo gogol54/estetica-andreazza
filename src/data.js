66ef54faf80ca7f3562fdd16f406a2cde9ee6222
gogol54 <uniuffs@gmail.com>
2023-09-01 17:23:30 -0300

    update faq page

--- src/data.js
+++ src/data.js
@@ -1,4 +1,11 @@
 export const dataset = []
+const tag = (
+	<>
+		<span>Sim, existe uma coleção abrangente de exemplos de antes e depois de procedimentos anteriores disponíveis na página do Instagram: </span>
+		<a href="https://www.instagram.com/esteticantoniandreazza/">@esteticantoniandreazza</a>
+		<span>. Isso permite que você visualize as transformações incríveis que os tratamentos proporcionaram aos clientes.</span>
+	</>
+)
 
 export const faq = [
 	{
@@ -16,32 +23,32 @@ export const faq = [
 	},
 	{
 		question: 'Qual é o tempo de recuperação típico para os diferentes tratamentos?',
-		reply: 'O tempo de recuperação varia dependendo do tipo de tratamento realizado e também da sensibilidade da pele, entretanto, é necessário uma avaliação cuidadosa para as necessidades individuais de cada cliente para fornecer detalhes sobre o tempo de recuperação associado a cada procedimento.',
+		reply: 'O tempo de recuperação varia dependendo do tipo de tratamento realizado e também da sensibilidade da pele, entretanto, é necessário uma avaliação cuidadosa para as necessidades individuais de cada cliente. Só assim posso estar fornecendo mais detalhes sobre o tempo de recuperação associado a cada procedimento.',
 		target: '#collapseThree',
 		id: 'collapseThree'
 	},
 	{
 		question: 'Vocês têm exemplos de antes e depois de procedimentos anteriores?',
-		reply: 'Sim, existe uma coleção abrangente de exemplos de antes e depois de procedimentos anteriores disponíveis na página do Instagram: @esteticantoniandreazza. Isso permite que você visualize as transformações incríveis que os tratamentos proporcionaram aos clientes.',
+		reply: tag,
 		target: '#collapseFour',
 		id: 'collapseFour'
 	},
 	{
 		question: 'Como é determinado o plano de tratamento personalizado para minhas necessidades?',
 		reply: 'Nossa abordagem personalizada começa com uma avaliação minuciosa, na qual você pode expressar suas preocupações. Com base nessa avaliação, assim que identificado suas necessidades individuais será desenvolvido um plano de tratamento personalizado que aborde eficazmente suas metas estéticas.',
-    target: '#collapseFive',
+		target: '#collapseFive',
 		id: 'collapseFive'
 	},
 	{
 		question: 'Quais são as opções de financiamento ou pagamento para os procedimentos?',
 		reply: 'Aceitamos diversas opções para pagamento, incluindo dinheiro, Pix e a possibilidade de parcelamento em até 12x no cartão, com uma taxa correspondente.',
-    target: '#collapseSix',
+		target: '#collapseSix',
 		id: 'collapseSix'
 	},
 	{
 		question: 'Qual é a política de cancelamento e reagendamento?',
 		reply: 'Eu compreendo que imprevistos podem ocorrer. Caso você precise cancelar ou reagendar, peço que me avise com pelo menos 24 horas de antecedência. Não será aplicada nenhuma taxa em tais casos.',
-    target: '#collapseSeven',
+		target: '#collapseSeven',
 		id: 'collapseSeven'
-	},
+	}, 
 ]
